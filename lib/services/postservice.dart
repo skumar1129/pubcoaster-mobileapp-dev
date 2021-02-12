18bc8b85c0fb85eb30fd3ffe0962ff42d57d29d7
Sahil <sahilkumar1129@gmail.com>
2021-02-11 20:22:48 -0500

    Finish api service

--- lib/services/postservice.dart
+++ lib/services/postservice.dart
@@ -1,25 +1,99 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 import 'config.dart';
 import 'package:NewApp/models/feedpost.dart';
 import 'package:NewApp/models/singlepost.dart';
 import 'dart:async';
+
+List<FeedPost> parseFeedPosts(dateItems) {
+  var response =
+      dateItems.map<FeedPost>((json) => FeedPost.fromJson(json)).toList();
+  return response;
+}
+
+SinglePost parseSinglePost(dataItem) {
+  var response = SinglePost.fromJson(dataItem);
+  return response;
+}
 
 class PostService {
   // TODO: Add auth token in header for all calls (will do when firebase is implemented)
 
   Future<bool> addPost(item) async {
     String endpoint = '${Config.localUrl}/post';
+    // TODO: Add user from local storage
+    var reqBody = {
+      'username': item['username'],
+      'anonymous': item['anonymous'],
+      'location': item['location'],
+      'bar': item['bar'],
+      'rating': item['rating'],
+      'neighborhood': item['nbhood'],
+      'description': item['description'],
+      'pickLink': ''
+    };
+
+    bool succeed;
+    try {
+      await http.post(endpoint, body: jsonEncode(reqBody));
+      succeed = true;
+    } catch (e) {
+      print(e);
+      succeed = false;
+    }
+
+    return succeed;
   }
 
-  Future<bool> updatePost(String uuid) async {
+  Future<bool> updatePost(String uuid, item) async {
     String endpoint = '${Config.localUrl}/post/$uuid';
+    var reqBody = {
+      'pickLink': '',
+      'neighborhood': item['nbhood'],
+      'rating': item['rating'],
+      'bar': item['bar'],
+      'description': item['description']
+    };
+
+    bool succeed;
+
+    try {
+      await http.patch(endpoint, body: jsonEncode(reqBody));
+      succeed = true;
+    } catch (e) {
+      print(e);
+      succeed = false;
+    }
+
+    return succeed;
   }
 
   Future<bool> deletePost(String uuid) async {
     String endpoint = '${Config.localUrl}/post/$uuid';
+    bool succeed;
+
+    try {
+      await http.delete(endpoint);
+      succeed = true;
+    } catch (e) {
+      print(e);
+      succeed = false;
+    }
+
+    return succeed;
   }
 
   Future<SinglePost> getPost(String uuid) async {
     String endpoint = '${Config.localUrl}/post/$uuid';
+    var response;
+    try {
+      response = await http.get(endpoint);
+    } catch (e) {
+      print(e);
+    }
+
+    return compute(parseSinglePost, response);
   }
 
   Future<List<FeedPost>> getLocationPosts(String location, [int page]) async {
@@ -27,6 +101,14 @@ class PostService {
     if (page != null && page > 1) {
       endpoint += '?offset=$page';
     }
+    var response;
+    try {
+      response = await http.get(endpoint);
+    } catch (e) {
+      print(e);
+    }
+
+    return compute(parseFeedPosts, response);
   }
 
   Future<List<FeedPost>> getLocBarPosts(
@@ -35,6 +117,13 @@ class PostService {
     if (page != null && page > 1) {
       endpoint += '?offset=$page';
     }
+    var response;
+    try {
+      response = await http.get(endpoint);
+    } catch (e) {
+      print(e);
+    }
+    return response;
   }
 
   Future<List<FeedPost>> getLocNbhoodPosts(
@@ -43,6 +132,14 @@ class PostService {
     if (page != null && page > 1) {
       endpoint += '?offset=$page';
     }
+    var response;
+    try {
+      response = await http.get(endpoint);
+    } catch (e) {
+      print(e);
+    }
+
+    return response;
   }
 
   Future<List<FeedPost>> getLocUserPosts(
@@ -51,6 +148,14 @@ class PostService {
     if (page != null && page > 1) {
       endpoint += '?offset=$page';
     }
+    var response;
+    try {
+      response = await http.get(endpoint);
+    } catch (e) {
+      print(e);
+    }
+
+    return response;
   }
 
   Future<List<FeedPost>> getMyPosts(int page) async {
@@ -58,6 +163,14 @@ class PostService {
     if (page != null && page > 1) {
       endpoint += '?offset=$page';
     }
+    var response;
+    try {
+      response = await http.get(endpoint);
+    } catch (e) {
+      print(e);
+    }
+
+    return response;
   }
 
   Future<List<FeedPost>> getUserPosts(String user, int page) async {
@@ -65,5 +178,13 @@ class PostService {
     if (page != null && page > 1) {
       endpoint += '?offset=$page';
     }
+    var response;
+    try {
+      response = await http.get(endpoint);
+    } catch (e) {
+      print(e);
+    }
+
+    return response;
   }
 }
