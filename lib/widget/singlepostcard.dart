14df04fcdcba4699a5726349be69cb44092ee9f8
Sonali Kumar <sonalikumar@Sonalis-MacBook-Pro.local>
2021-02-28 19:28:28 -0500

    uber frustrating

--- lib/widget/singlepostcard.dart
+++ lib/widget/singlepostcard.dart
@@ -40,6 +40,9 @@ class SinglePostCard extends StatefulWidget {
 }
 
 class _SinglePostCardState extends State<SinglePostCard> {
+
+  String newComment;
+
   @override
   Widget build(BuildContext context) {
     String goodContent = utf8.decode(widget.description.codeUnits);
@@ -48,31 +51,41 @@ class _SinglePostCardState extends State<SinglePostCard> {
     var newDate = HttpDate.parse(widget.timestamp);
     // TODO: Look into better way to get real time
     var date = newDate.add(Duration(hours: 5));
-    return Card(
+    return Container(
+        height: MediaQuery.of(context).size.height * .6,
+        child: Card(
         color: Colors.black,
         child: Column(
           children: [
+            const Divider(
+              color: Colors.black,
+              thickness: 1,
+            ),
             // ListTile(
             Row(
               children: [
-                Flexible(
-                  child: Text(
-                    capitalize(goodBar),
-                    style: TextStyle(
-                        color: Colors.white, fontFamily: 'Merriweather-Bold'),
-                  ),
-                ),
-                Flexible(
+                Padding(
+                  padding: const EdgeInsets.only(left: 4),
                     child: Text(
-                  'User Rating: ${widget.rating}/10',
-                  style: TextStyle(
-                      color: Colors.white, fontFamily: 'Merriweather-Regular'),
-                ))
+                      capitalize(goodBar),
+                      style: TextStyle(
+                          color: Colors.white, fontFamily: 'Merriweather-Bold', fontSize: 20),
+                    ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 4),
+                  child: Text(
+                    'User Rating: ${widget.rating}/10',
+                    style: TextStyle(
+                        color: Colors.white, fontFamily: 'Merriweather-Regular', fontSize: 15),
+                  ),
+                )
               ],
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
             ),
             // ),
             const Divider(
+              thickness: 1,
               color: Colors.white,
             ),
             (() {
@@ -80,6 +93,7 @@ class _SinglePostCardState extends State<SinglePostCard> {
                 return Column(children: [
                   Image(image: NetworkImage('${widget.picLink}')),
                   const Divider(
+                    thickness: 1,
                     color: Colors.white,
                   )
                 ]);
@@ -90,20 +104,23 @@ class _SinglePostCardState extends State<SinglePostCard> {
             Text(
               goodContent,
               style: TextStyle(
-                  color: Colors.white, fontFamily: 'Merriweather-Regular'),
+                  color: Colors.white, fontFamily: 'Merriweather-Regular', fontSize: 25),
             ),
             const Divider(
               color: Colors.white,
+              thickness: 1,
             ),
             Row(
               children: [
                 (() {
                   if (widget.username != null) {
                     String goodUsername = utf8.decode(widget.username.codeUnits);
-                    return Flexible(
+                    return Padding(
+                      padding: EdgeInsets.only(left: 4),
                       child: Text(
                         '$goodUsername',
                         style: TextStyle(
+                            fontSize: 18,
                             color: Colors.white,
                             fontFamily: 'Merriweather-Regular'),
                       ),
@@ -114,26 +131,32 @@ class _SinglePostCardState extends State<SinglePostCard> {
                 }()),
                 (() {
                   if (widget.likes.length == 0) {
-                    return Flexible(
+                    return Padding(
+                        padding: EdgeInsets.only(right: 4),
                         child: Text(
                       'No Likes Yet',
                       style: TextStyle(
+                          fontSize: 18,
                           color: Colors.white,
                           fontFamily: 'Merriweather-Regular'),
                     ));
                   } else if (widget.likes.length == 1) {
-                    return Flexible(
+                    return Padding(
+                      padding: EdgeInsets.only(right: 4),
                         child: Text(
                       '${widget.likes.length} like',
                       style: TextStyle(
+                          fontSize: 18,
                           color: Colors.white,
                           fontFamily: 'Merriweather-Regular'),
                     ));
                   } else {
-                    return Flexible(
+                    return Padding(
+                      padding: EdgeInsets.only(right: 4),
                         child: Text(
                       '${widget.likes.length} likes',
                       style: TextStyle(
+                          fontSize: 18,
                           color: Colors.white,
                           fontFamily: 'Merriweather-Regular'),
                     ));
@@ -144,35 +167,39 @@ class _SinglePostCardState extends State<SinglePostCard> {
             ),
             const Divider(
               color: Colors.black,
-              thickness: 0.5,
+              thickness: 1,
             ),
             Row(
               children: [
-                Flexible(
+                Padding(
+                  padding: EdgeInsets.only(left: 4),
                   child: Text(
                     timeago.format(date.toLocal()),
                     style: TextStyle(
-                        color: Colors.white, fontFamily: 'Merriweather-Italic'),
+                        color: Colors.white, fontFamily: 'Merriweather-Italic', fontSize: 18),
                     softWrap: true,
                   ),
                 ),
                 (() {
                   if (widget.neighborhood != null) {
                     String goodNbhood = utf8.decode(widget.neighborhood.codeUnits);
-                    return Flexible(
+                    return Padding(
+                      padding: EdgeInsets.only(right: 4),
                         child: Text(
                       '${capitalize(goodNbhood)}, $goodLocation',
                       style: TextStyle(
                           color: Colors.white,
-                          fontFamily: 'Merriweather-Regular'),
+                          fontFamily: 'Merriweather-Regular', fontSize: 18),
                       softWrap: true,
                     ));
                   } else {
-                    return Flexible(
+                    return Padding(
+                      padding: EdgeInsets.only(right: 4),
                         child: Text(
                       '$goodLocation',
                       style: TextStyle(
                           color: Colors.white,
+                          fontSize: 18,
                           fontFamily: 'Merriweather-Regular'),
                       softWrap: true,
                     ));
@@ -183,44 +210,92 @@ class _SinglePostCardState extends State<SinglePostCard> {
             ),
             const Divider(
               color: Colors.white,
+              thickness: 1,
+            ),
+            (() {
+              if (widget.comments.length == 0) {
+                return new Row(children:[ 
+                  Center(
+                    child: Text(
+                    'No comments yet',
+                    style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Merriweather-Regular'),
+                ))]
+                );
+              } else {
+                  return Expanded(
+                    child: ListView.separated(
+                    padding: EdgeInsets.all(1),
+                    itemCount: widget.comments.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      var newDate = HttpDate.parse(widget.comments[index]['createdAt']);
+                      // TODO: Look into better way to get real time
+                      var date = newDate.add(Duration(hours: 5));
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left: 4),
+                            child: Text(
+                              "${widget.comments[index]['createdBy']} : ${widget.comments[index]['text']}",
+                              style: TextStyle(
+                                 color: Colors.white, fontFamily: 'Merriweather-Bold'),
+                          ),
+                         ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 4),
+                          child: Text(
+                          "${timeago.format(date.toLocal())}",
+                          style: TextStyle(
+                          color: Colors.white, fontFamily: 'Merriweather-Regular'),
+                        ))
+                        ],
+                      );
+                    },
+                    separatorBuilder: (BuildContext context, int index) => const Divider(color: Colors.blueGrey)
+                    ));
+              }
+            }()),
+            const Divider(
+              color: Colors.white,
+              thickness: 1,
             ),
             Row(
               children: [
-                (() {
-                  if (widget.comments.length == 0) {
-                    return Flexible(
-                        child: Text(
-                      'No comments yet',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Merriweather-Regular'),
-                    ));
-                  } else if (widget.comments.length == 1) {
-                    return Flexible(
-                        child: Text(
-                      '${widget.comments.length} comment',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Merriweather-Regular'),
-                    ));
-                  } else {
-                    return Flexible(
-                        child: Text(
-                      '${widget.comments.length} comments',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Merriweather-Regular'),
-                    ));
-                  }
-                }())
+                Expanded(child:
+                  Padding(
+                    padding: EdgeInsets.only(left: 4),
+                    child: TextField(
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        labelText: 'Comment',
+                        border: OutlineInputBorder(),
+                        isDense: true,
+                        contentPadding: EdgeInsets.all(8),
+                      ),
+                      onChanged: (String value) {
+                        newComment = value;
+                      }
+                ),
+                  )),
+                Expanded(child:
+                  Icon(
+                    Icons.send,
+                    color: Colors.grey,
+                    size: 24.0,
+                    semanticLabel: 'Send your comment',
+                )),
               ],
             ),
             const Divider(
               color: Colors.black,
-              thickness: 0.5,
-            ),
+              thickness: 1,
+            )
           ],
         ),
-      );
+      ));
   }
+
+ 
 }
